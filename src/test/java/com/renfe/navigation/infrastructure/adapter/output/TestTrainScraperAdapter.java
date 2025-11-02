5ac4aecee55706e59a47bcc9296f9a0a2119f296
David de la Rosa <drd_id@hotmail.com>
2025-11-02 11:31:40 +0100

    feat: add Playwright test profiles and update .gitignore for node_modules

--- src/test/java/com/renfe/navigation/infrastructure/adapter/output/TestTrainScraperAdapter.java
+++ src/test/java/com/renfe/navigation/infrastructure/adapter/output/TestTrainScraperAdapter.java
@@ -19,7 +19,7 @@ public class TestTrainScraperAdapter implements TrainScraperPort {
     public List<List<Train>> scrapeTrains(String origin, String destination, String dateOut, String dateReturn, int adults) {
         Train t1 = new Train("T123", "AVE", "08:00", "10:30", "2h30m", 29.99);
         Train t2 = new Train("T456", "ALVIA", "09:00", "12:30", "3h30m", 19.99);
-                List<Train> out = List.of(t1, t2);t(t1, t2));
+        List<Train> outBoundTrains = new ArrayList<>(Arrays.asList(t1, t2));
         List<Train> returnTrains = new ArrayList<>();
         return Arrays.asList(outBoundTrains, returnTrains);
     }
